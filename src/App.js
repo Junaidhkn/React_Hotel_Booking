1923e2cb88c3081a14f78373d97fa2593e366c2c
Junaidhkn <junaidhkniazi@gmail.com>
2022-11-05 00:37:16 +0500

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './pages/home/Home'
+import List from './pages/list/List'
+import HotelDetail from './pages/hotel/HotelDetail'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<HotelDetail />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
