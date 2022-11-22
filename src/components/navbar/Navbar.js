f96f87fe32a215b78a4150b5d9b2eaacbdd35cad
Junaidhkn <junaidhkniazi@gmail.com>
2022-11-23 00:07:07 +0500

    commit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,23 +1,27 @@
-import './navbar.css'
+import './navbar.css';
+import { Link } from 'react-router-dom';
 
-const navbar = ( { type } ) => {
-   return (
-      <>
-         <div className={
-            type === 'list' ? 'navbar listMode' : 'navbar'
-         }>
-            <div className={
-               type === 'list' ? 'navContainer listMode' : 'navContainer'
-            }>
-               <span className="logo">Bookie</span>
-               <div className="navItems">
-                  <button className="navButton">Register</button>
-                  <button className="navButton">Log In</button>
-               </div>
-            </div>
-         </div>
-      </>
-   )
-}
+const navbar = ({ type }) => {
+	return (
+		<>
+			<div className={type === 'list' ? 'navbar listMode' : 'navbar'}>
+				<div
+					className={
+						type === 'list' ? 'navContainer listMode' : 'navContainer'
+					}>
+					<Link
+						to='/'
+						style={{ color: 'inherit', textDecoration: 'none' }}>
+						<span className='logo'>Bookie</span>
+					</Link>
+					<div className='navItems'>
+						<button className='navButton'>Register</button>
+						<button className='navButton'>Log In</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
 
-export default navbar
+export default navbar;
