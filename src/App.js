8a5820a3ea24696ec20e5f4ba4e3b00dd6af2f47
Junaidhkn <junaidhkniazi@gmail.com>
2022-11-28 04:42:58 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 import Home from './pages/home/Home'
 import List from './pages/list/List'
 import HotelDetail from './pages/hotel/HotelDetail'
+import Login from "./pages/login/Login.js";
 
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/hotels" element={<List />} />
         <Route path="/hotels/:id" element={<HotelDetail />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
